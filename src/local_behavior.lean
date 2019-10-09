96ec67e4ee3938e91aac1803c6f41f781539aef9
Jesse Michael Han <hyrodi@gmail.com>
2019-10-09 15:37:40 -0400

    feat (src/local_behavior): start on showing det = Det
    chore(src/local_behavior): interchange Det and Det₂

--- src/local_behavior.lean
+++ src/local_behavior.lean
@@ -8,6 +8,33 @@ noncomputable theory
 
 universes u v w u' v' w'
 
+namespace finset
+
+section sum_congr₂
+
+open_locale classical
+
+variables {α : Type u} {α' : Type u'} {β : Type v} {s₁ : finset α} {s₂ : finset α'} {f : α → β} {g : α' → β} [add_comm_monoid β]
+
+lemma sum_congr₂ (ϕ : (↑s₁ : set α) → (↑s₂ : set α')) (Hϕ : function.bijective ϕ) (H_eq : ∀ (x : (↑s₁ : set α)), f x = g (ϕ x)) : s₁.sum f = s₂.sum g :=
+begin
+  revert s₂ f g ϕ Hϕ H_eq, apply finset.induction_on s₁,
+    { intros s₂ f g ϕ H_bih H_eq, suffices : s₂ = ∅, by subst this; simp,
+      ext, refine ⟨_,_⟩; intro H,
+        { cases H_bih.right ⟨a, by simp*⟩, repeat{auto_cases} },
+        { cases H }
+      },
+    { intros a s H_not_mem IH s₂ f g ϕ Hϕ H_eq,
+      rw sum_insert ‹_›, specialize @IH (s₂.erase $ ϕ ⟨a, by tidy⟩) f g _ _,
+      rw IH, have := H_eq ⟨a, _⟩, dsimp at this, rw this,
+      repeat {sorry} }
+end
+
+end sum_congr₂
+
+end finset
+
+
 namespace finsupp
 
 section from_finset
@@ -41,6 +68,33 @@ section Det
 open_locale classical
 
 open equiv equiv.perm finsupp
+variables {α : Type u} [comm_ring α] {σ : Type w} [fintype σ]
+
+def indeterminate_matrix : matrix σ σ $ mv_polynomial (σ × σ) α :=
+λ i j, mv_polynomial.X (i, j)
+
+def Det : mv_polynomial (σ × σ) α := det indeterminate_matrix
+
+def valuation_of_matrix (M : matrix σ σ α) : σ × σ → α :=
+λ ⟨i,j⟩, M i j
+
+-- TODO: characterize Det.support
+lemma Det_eval (M : matrix σ σ α) : Det.eval (λ ⟨i,j⟩, M i j) = det M :=
+begin
+  unfold det, unfold mv_polynomial.eval mv_polynomial.eval₂ finsupp.sum,
+  refine finset.sum_congr₂ _ _ _,
+    { sorry },
+    { sorry },
+    { sorry }
+end
+
+end Det
+
+section Det₂
+
+open_locale classical
+
+open equiv equiv.perm finsupp
 variables {α : Type u} [comm_ring α] {X : Type w} [fintype X]
 
 instance : fintype (perm X) := sorry -- by apply_instance -- ?
@@ -53,27 +107,13 @@ def perm_term (σ : perm X) : (X × X) →₀ ℕ :=
 -- do we already know that every unit of ℤ is 1 or -1?
 def unit_coe : units ℤ → units α := sorry
 
-def Det : mv_polynomial (X × X) α :=
+def Det₂ : mv_polynomial (X × X) α :=
 from_finset
   (finset.image perm_term (finset.univ : finset $ perm X))
   begin
     rintro ⟨x,Hx⟩, simp at Hx,
     exact (unit_coe $ sign $ classical.some Hx).val
   end
-
-end Det
-
-section Det₂
-
-open_locale classical
-
-open equiv equiv.perm finsupp
-variables {α : Type u} [comm_ring α] {σ : Type w} [fintype σ]
-
-def indeterminate_matrix : matrix σ σ $ mv_polynomial (σ × σ) α :=
-λ i j, mv_polynomial.X (i, j)
-
-def Det₂ : mv_polynomial (σ × σ) α := det indeterminate_matrix
 
 end Det₂
 
@@ -92,13 +132,13 @@ variables
   [topological_semiring α]
   [topological_semiring β]
 
-lemma continuous_eval₂ (ϕ : α → β) (ϕ_cts : continuous ϕ) (p : mv_polynomial σ α) : continuous (λ v : σ → β, p.eval₂ ϕ v) :=
+lemma continuous_eval₂ (ϕ : α → β) [is_semiring_hom ϕ] (p : mv_polynomial σ α) : continuous (λ v : σ → β, p.eval₂ ϕ v) :=
 begin
   sorry
 end
 
 lemma continuous_eval (p : mv_polynomial σ α) : continuous (λ v : σ → α, p.eval v) :=
-continuous_eval₂ id continuous_id p
+continuous_eval₂ id p
 
 end continuous_eval
 
