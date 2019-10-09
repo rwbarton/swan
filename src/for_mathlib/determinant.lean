9a1d1d7c4a0e1fedfadf5e18b1628e786dbe52f2
Reid Barton <rwbarton@gmail.com>
2019-10-09 17:58:45 -0400

    add a TODO

--- src/for_mathlib/determinant.lean
+++ src/for_mathlib/determinant.lean
@@ -13,6 +13,7 @@ variables {α : Type u} [comm_ring α] {σ : Type w} [fintype σ] [decidable_eq 
 def Det : mv_polynomial (σ × σ) α := det (λ i j, mv_polynomial.X (i, j))
 
 lemma Det_eval (M : matrix σ σ α) : Det.eval (λ ⟨i,j⟩, M i j) = det M :=
+-- TODO: There should be simp lemmas for `mv_polynomial.eval` so we don't have to unfold it here
 by { unfold Det det mv_polynomial.eval, simp }
 
 end Det
